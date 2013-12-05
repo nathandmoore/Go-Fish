72eb853bc61366ca06f10bf249fc3ecbdb0241fb
Nathan Moore <moorednathan@gmail.com>
2013-12-05 10:17:37 -0600

    Finished underlying infrastructure for cards and deck. Now stores whether each card is a face card, ace, and the drawable for the face, and the suit as an integer as well as the suit drawable.

--- GoFishMoore/src/com/cse3345/f13/moore/MainActivity.java
+++ GoFishMoore/src/com/cse3345/f13/moore/MainActivity.java
@@ -12,30 +12,119 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private Drawable[] mSuits;
+	private Drawable[] mFaces;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
         Resources res = getResources();
+        
         /** Begin Game Logic */
+        
         //get suit drawables
         TypedArray suitsIDs = res.obtainTypedArray(R.array.cardSuits);
-        BitmapDrawable[] suits = new BitmapDrawable[suitsIDs.length()];
+        mSuits = new Drawable[suitsIDs.length()];
         for (int index = 0; index < suitsIDs.length(); index++) {
-        	suits[index] = (BitmapDrawable) suitsIDs.getDrawable(index);
+        	mSuits[index] = suitsIDs.getDrawable(index);
         }
-        Deck deck = new Deck(suits);
+        
+        //get face card drawables
+        TypedArray faceCardIDs = res.obtainTypedArray(R.array.faceCards);
+        mFaces = new Drawable[faceCardIDs.length()];
+        for (int index = 0; index < faceCardIDs.length(); index++) {
+        	mFaces[index] = faceCardIDs.getDrawable(index);
+        }
+        
+        //create deck
+        Deck deck = new Deck();
         
     }
     
     private class Card {
     	public String value;
-    	public Drawable suit;
+    	public Drawable suitImg;
+    	public int suit;
+    	public boolean isFaceCard;
+    	public boolean isAce;
+    	public Drawable face;
     	
-    	public Card(Drawable s, String v) {
-    		suit = s;
-    		value = v;    		
+    	public Card(int insuit, Drawable insuitImg, String invalue, boolean inisFaceCard, boolean inisAce) {
+    		
+    		suit = insuit;
+    		suitImg = insuitImg;
+    		value = invalue;    
+    		isFaceCard = inisFaceCard;
+    		isAce = inisAce;
+    		setFace();
+    	}
+    	
+    	private void setFace() {
+    		
+    		if (isFaceCard) {
+    			
+    			if (value.equals("K")) { //king
+    				
+    				if (suit == 1) { //hearts
+    					
+    					face = mFaces[0];
+    					
+    				} else if (suit == 2) { //clubs
+    					
+    					face = mFaces[2];
+    					
+    				} else if (suit == 3) { //diamonds
+    					
+    					face = mFaces[4];
+    					
+    				} else { //spades
+    					
+    					face = mFaces[6];
+    					
+    				}
+    				
+    			} else if (value.equals("Q")) { //queen
+    				
+    				
+    				
+    				if (suit == 1) { //hearts
+    					
+    					face = mFaces[1];
+    					
+    				} else if (suit == 2) { //clubs
+    					
+    					face = mFaces[3];
+    					
+    				} else if (suit == 3) { //diamonds
+    					
+    					face = mFaces[5];
+    					
+    				} else { //spades
+    					
+    					face = mFaces[7];
+    					
+    				}
+    				
+    			} else if (value.equals("J")) { //jack
+    				
+    				
+    				
+    				if (suit == 1 || suit == 3) { //hearts and diamonds: red
+    					
+    					face = mFaces[9];
+    					
+    				} else { //clubs and spades: black
+    					
+    					face = mFaces[8];
+    					
+    				}
+    				
+    			}
+    			
+    		}
+    		
     	}
     }
     
@@ -50,35 +139,49 @@ public class MainActivity extends Activity {
     
     private class Deck {
     	public ArrayList<Card> cards;
-    	public BitmapDrawable[] suits;
     	
-    	public Deck(BitmapDrawable[] b) {
+    	public Deck() {
     		cards = new ArrayList<Card>(52);
-    		suits = new BitmapDrawable[b.length];
-    		System.arraycopy(b, 0, suits, 0, b.length);
     		generateDeck();
     	}
     	
     	private void generateDeck() {
     		
     		String value;
+    		boolean isFaceCard = true;
+    		boolean isAce = false;
+    		Drawable face;
     		
-    		for (int suit = 0; suit < suits.length; suit++) {    				
+    		for (int suit = 0; suit < mSuits.length; suit++) {    				
     			
     			for (int card = 1; card < 14; card++) {
     				
-    				if (card == 1)
+    				if (card == 1) {
+    					
     					value = "A";
-    				else if (card == 11)
+    					isAce = true;
+    					isFaceCard = false;    							
+    					
+    				} else if (card == 11) {
+    					
     					value = "J";
-    				else if (card == 12)
+    					
+    				} else if (card == 12) {
+    					
     					value = "Q";
-    				else if (card == 13)
+    					
+    				} else if (card == 13) {
+    					
     					value = "K";
-    				else
+    					
+    				} else {
+    					
     					value = String.valueOf(card);
+    					isFaceCard = false;
+    					
+    				}
     				
-    				Card newCard = new Card(suits[suit],value);
+    				Card newCard = new Card(suit+1, mSuits[suit], value, isFaceCard, isAce);
     				cards.add(newCard);
     			}
     			
